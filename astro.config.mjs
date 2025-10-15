73737953739467501e18f453704344c78b17b91c
Andrew Henrice <contatodigidrive@gmail.com>
2025-10-15 15:44:26 +0900

    Update GitHub Pages workflow and site config
    Refactored the GitHub Actions workflow for deploying to GitHub Pages, introducing a separate build job and updating steps for Astro. Updated Astro config to use the correct site URL and base path for GitHub Pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://dawnwebs.com",
+  site: "https://dawnmedia.github.io/DawnWeb",
+  base: "/DawnWeb/",
+
   image: {
     domains: ["images.unsplash.com"],
   },
